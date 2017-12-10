61e4c051f7195dfb9daa1de2c806da67f0e09bc1
Emily Carey <emilyrosinacarey@gmail.com>
2017-12-10 01:12:28 +0000

    tweaked app and body styles

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
@@ -11,12 +11,19 @@ export default {
 </script>
 
 <style>
-#app {
+  body,
+  #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-}
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100vw;
+  background-color: #00bbff;
+  }
 </style>
