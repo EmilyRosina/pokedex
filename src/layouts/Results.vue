ec73c225e57e61907bc30b22e6de41be11ce15c7
Emily Carey <emilyrosinacarey@gmail.com>
2017-12-10 01:46:43 +0000

    setup layout components

---
+++ src/layouts/Results.vue
@@ -0,0 +1,41 @@
+<template>
+  <el-container>
+    <el-header>
+      <el-button @click="newSearch">New Search</el-button>
+      <el-button @click="goToRandomPokemon">Random</el-button>
+    </el-header>
+    <el-main>
+      <!-- CONTENT -->
+      <slot></slot>
+    </el-main>
+    <el-footer>4uroraSkye &copy; {{ new Date().getFullYear()}}</el-footer>
+  </el-container>
+</template>
+
+<script>
+  export default {
+    methods: {
+      clearSearch () {
+        this.commit('CLEAR_SEARCH')
+      },
+      goToRandomPokemon () {
+        let randomId = Math.floor(Math.random() * (151) + 1)
+        this.$router.push({ params: { id: randomId } })
+      },
+      newSearch () {
+        this.$router.push('/')
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .el-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .el-button {
+      width: 10em;
+    }
+  }
+</style>
---
+++ src/layouts/Search.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-container>
+    <el-main>
+      <el-button @click="goToRandomPokemon">Random</el-button>
+      <!-- CONTENT -->
+      <slot></slot>
+    </el-main>
+    <el-footer>4uroraSkye &copy; {{ new Date().getFullYear()}}</el-footer>
+  </el-container>
+</template>
+
+<script>
+  export default {
+    methods: {
+      clearSearch () {
+        this.commit('CLEAR_SEARCH')
+      },
+      goToRandomPokemon () {
+        let randomId = Math.floor(Math.random() * (151) + 1)
+        this.$router.push({ name: 'Pokemon', params: { id: randomId } })
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .el-main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
