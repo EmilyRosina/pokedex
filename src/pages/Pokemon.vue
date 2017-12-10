d3eeda3a008364f9d44aa158662d812e50dca15c
Emily Carey <emilyrosinacarey@gmail.com>
2017-12-10 01:46:50 +0000

    setup page components

---
+++ src/pages/Pokemon.vue
@@ -0,0 +1,29 @@
+<template>
+  <results-layout>
+    <el-row>
+      <el-col>
+        Pokemon! #{{ pokemonId }}
+      </el-col>
+
+    </el-row>
+  </results-layout>
+</template>
+
+<script>
+  import resultsLayout from '@/layouts/Results'
+
+  export default {
+    components: {
+      resultsLayout
+    },
+    computed: {
+      pokemonId () {
+        return this.$route.params.id
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/pages/Search.vue
@@ -0,0 +1,26 @@
+<template>
+  <search-layout>
+    <el-row>
+      <el-col type="flex">Search!</el-col>
+    </el-row>
+  </search-layout>
+</template>
+
+<script>
+  import searchLayout from '@/layouts/Search.vue'
+
+  export default {
+    components: {
+      searchLayout
+    },
+    computed: {
+      randomId () {
+        return Math.floor(Math.random() * (151) + 1)
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
