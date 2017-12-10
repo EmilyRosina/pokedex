f3b870ccfdfb79206b5656c4166fe08e1ec68bfd
Emily Carey <emilyrosinacarey@gmail.com>
2017-12-10 15:56:39 +0000

    added json for testing (so not to exhaust api calls)

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,11 +1,21 @@
+const json = true
+
+const POKEAPI = (param) => { return json ? `../../static/json/pokeapi/${param}.json` : `https://pokeapi.co/api/v2/${param}` }
+const TCGAPI = (param) => { return json ? `../../static/json/tcg/${param}.json` : `https://api.pokemontcg.io/v1/${param}` }
+
 export default {
-  pokeapi () {
+  api () {
     return {
-      allPokemonData: 'https://pokeapi.co/api/v2/generation/1/' // pokemon_species, types
+      pokeapi: {
+        generationById: (id) => { return POKEAPI(`generation/${id}`) },
+        pokemonById: (id) => { return POKEAPI(`pokemon/${id}`) },
+        pokemonSpeciesById: (id) => { return POKEAPI(`pokemon-species/${id}`) },
+        typeById: (id) => { return POKEAPI(`type/${id}`) }
+      },
+      tgc: {
+        pokemonById: (state) => (id) => { return `${TCGAPI}cards?nationalPokedexNumber=${id}` }
     }
-  },
-  tcgApiPokemonById: (state) => (id) => {
-    return `https://api.pokemontcg.io/v1/cards?nationalPokedexNumber=${id}`
+    }
   },
   pokemonById: (state) => (id) => {
     return state.pokedex ? state.pokedex.find(pokemon => pokemon.id === id) : null
